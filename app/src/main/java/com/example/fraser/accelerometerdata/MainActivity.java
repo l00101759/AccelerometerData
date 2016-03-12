b42f5a39e6c21fe0941ac893876ea6f8491fdfa5
Fraser Cassidy <l00101759@student.lyit.ie>
2016-03-12 18:59:08 +0000

    DTW
    Compares float arrays
    
    I'm comparing previously recorded y and z float values
    against single y and z values on sensor changed.
    
    Getting decent readings. Closer to 0 the return value is the better it is.

--- app/src/main/java/com/example/fraser/accelerometerdata/MainActivity.java
+++ app/src/main/java/com/example/fraser/accelerometerdata/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.fraser.accelerometerdata;
 
 import android.content.Context;
 import android.content.Intent;
+import android.content.res.Configuration;
 import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
@@ -19,16 +20,21 @@ import java.io.FileOutputStream;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity implements SensorEventListener{
 
     private Sensor mySensor;
     private SensorManager SM;
 
+    Values v = new Values();
+    private static ArrayList <Values> values = new ArrayList<Values>(10000);
+    static DTW dtw;
+
     private float[] gravity = new float[3];
     private float[] linear_acceleration = new float[3];
 
-    private TextView x,y,z, action;
+    private TextView x,y,z, action, txtOrientation;
 
     ImageView image;
 
@@ -48,13 +54,19 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
         y = (TextView)findViewById(R.id.yText);
         z = (TextView)findViewById(R.id.zText);
         action = (TextView)findViewById(R.id.txtAction);
+        txtOrientation = (TextView)findViewById(R.id.txtOrientation);
 
         image = (ImageView) findViewById(R.id.imgDirection);
         image.setVisibility(View.GONE);
 
+        if(getResources().getConfiguration().orientation == Configuration.ORIENTATION_PORTRAIT){
+            txtOrientation.setText("Device Orientation: Portrait" );
+        }
+        else {
+            txtOrientation.setText("Device Orientation: NOT PORTRAIT");
+        }
+
     }
-
-
 
     @Override
     public void onSensorChanged(SensorEvent event) {
@@ -75,12 +87,16 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
         float y1 =  linear_acceleration[1];
         float z1 =  linear_acceleration[2];
 
+
+
         x.setText("X: "+ x1);
         y.setText("Y: "+ y1);
-        z.setText("Z: "+ z1);
+        z.setText("Z: " + z1);
 
         if(y1 < -2 && z1 > 2)
         {
+            start(x1, y1, z1);
+
             action.setText("Phone Moved Forward!!");
             image.setVisibility(View.VISIBLE);
             image.setImageResource(R.drawable.forwardarrow);
@@ -125,6 +141,41 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
         super.onResume();
         SM.registerListener(this, mySensor, SensorManager.SENSOR_DELAY_NORMAL);
     }
+
+    protected void compassOpen(View view)
+    {
+        Intent intent = new Intent(this, CompassActivity2.class);
+
+    }
+
+    public void start(float x1, float y1, float z1 )
+    {
+        float[] yValues = {y1};//use one y reading from device
+        float[] zValues = {z1};//use one z reading from device
+
+        float y = v.getY();//get the stored reading of a forward gesture
+        float[] storedYValues = {y};
+
+        float z = v.getZ();//get the stored reading of a forward gesture
+        float[] storedZValues = {z};
+
+        //values.add((new Values(x1, y1, z1)));
+        //System.out.println("****** - X: " + x1 + " Y: " + y1 + " Z: " + z1);
+
+        System.out.println("**** DTW - Y - Reading ===  ");
+        System.out.println(dtw = new DTW(yValues, storedYValues));
+        System.out.println("**** DTW - Z - Reading ===  ");
+        System.out.println(dtw = new DTW(zValues, storedZValues));
+
+
+
+    }
+    public void stop()
+    {
+
+    }
+
+
 
 
 }
